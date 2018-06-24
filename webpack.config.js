48cac4ee5145ed3453f13083a428de9e7863b6d2
iamNitin16 <nitinsharma95ns@gmail.com>
2018-06-25 02:04:21 +0530

    fixed typo in webpack configuration file

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-	entry: "./src/app.js",
+	entry: "./src/index.js",
 	output: {
 		path: __dirname,
 		filename: "./public/bundle.js"
