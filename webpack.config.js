e9e87602c8b44ad3ec41cee6cb26fce6fa540f3a
iamNitin16 <nitinsharma95ns@gmail.com>
2018-06-25 01:43:28 +0530

    Added webpack configuration file

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+	entry: "./src/app.js",
+	output: {
+		path: __dirname,
+		filename: "./public/bundle.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{
+						loader: "style-loader"
+					},
+					{
+						loader: "css-loader",
+						options: {
+							modules: true,
+							importLoaders: 1,
+							localIdentName: "[name]_[local]_[hash:base64]",
+							sourceMap: true,
+							minimize: true
+						}
+					}
+				]
+			}
+		]
+	}
+};
