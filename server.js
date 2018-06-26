7d64bb36e80f7c57a196c360fab7141407437149
iamNitin16 <nitinsharma95ns@gmail.com>
2018-06-26 23:03:31 +0530

    fixed changes and added gh-pages to deploy react for user pages

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 var app = express();
 
-app.use(express.static("public"));
+app.use(express.static("build"));
 
 app.listen(8080, function() {
 	console.log("Express Server is up and running on port 8080");
